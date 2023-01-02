f9be85c660a9697cd178f9ecfcc207ee172a6ac2
Abpv <74953874+Abpv@users.noreply.github.com>
2023-01-02 10:05:02 +0100

    BR leer bd ok

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,18 @@
 <?php
+    /*CONEXION BASE DATOS*/
+    //importamos la conexion
+    require '../includes/config/database.php';
+    $db = conectarDB();
+    //escribimos query
+    $query = "SELECT * FROM propiedades";
+    //consultar bd
+    $rs = mysqli_query($db, $query);
+    
+
+    //muestra mensaje condicional
     $resultado = $_GET['resultado'] ?? null; //busca resultado y si no existe le asigna null
 
+    //incluye un template
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -10,8 +22,41 @@
             <p class="alerta exito">Anuncio Creado Correctamente</p>    
         <?php endif; ?>
         <a href="/bienesraices/admin/propiedades/crear.php" class="boton-verde">Nueva Propiedad</a>
+        
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <!--Mostrar resultados-->
+            <tbody>
+                <?php while($row = mysqli_fetch_assoc($rs)) : ?>
+
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['titulo']; ?></td>
+                    <td><img src="/bienesraices/imagenes/<?php echo $row['imagen'];?>" class="imagen-tabla"></td>
+                    <td><?php echo $row['precio']; ?></td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endwhile; ?>
+            </tbody>
+        </table>
     </main>
 
 <?php
+    //cerrar conexion
+    mysqli_close($db);
+
+    
     incluirTemplate('footer');
 ?>
